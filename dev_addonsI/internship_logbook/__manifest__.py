157aad9b8bde618b6d34007803ee4ce0769e5de7
Arda Alan <ardaalaann@gmail.com>
2026-07-28 15:01:08 +0300

    feat(website): redesign public footer

--- dev_addonsI/internship_logbook/__manifest__.py
+++ dev_addonsI/internship_logbook/__manifest__.py
@@ -53,6 +53,7 @@ This module allows companies to manage:
     "views/internship_frontend_components.xml",
     "views/internship_landing_templates.xml",
     "views/internship_navigation_templates.xml",
+    "views/internship_footer_templates.xml",
     "views/self_registration_templates.xml",
     "views/internship_portal_templates.xml",
 
