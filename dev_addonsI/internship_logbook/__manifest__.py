758209eb089b489d82b62aed1a44a8d365197673
Arda Alan <ardaalaann@gmail.com>
2026-07-28 12:08:04 +0300

    feat(website): add SaaS landing hero features and workflow

--- dev_addonsI/internship_logbook/__manifest__.py
+++ dev_addonsI/internship_logbook/__manifest__.py
@@ -51,6 +51,7 @@ This module allows companies to manage:
     "report/internship_report.xml",
 
     "views/internship_frontend_components.xml",
+    "views/internship_landing_templates.xml",
     "views/self_registration_templates.xml",
     "views/internship_portal_templates.xml",
 
